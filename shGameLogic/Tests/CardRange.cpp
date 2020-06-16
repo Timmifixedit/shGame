c00708b626d58891362c5f9406bd9b2943151636
Timmifixedit <tim.luchterhand@uni-ulm.de>
2020-06-16 20:18:46 +0200

    more CardRange tests

--- shGameLogic/Tests/CardRange.cpp
+++ shGameLogic/Tests/CardRange.cpp
@@ -61,5 +61,66 @@ TEST(card_range_test, policy_and_return) {
     EXPECT_EQ(game.getPolicies().find(policy)->second, 1);
     auto other = policy == sh::CardType::Fascist ? CardType::Liberal : CardType::Fascist;
     EXPECT_EQ(game.getPolicies().find(other)->second, 0);
+}
 
+TEST(car_range_test, discard_not_found) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    CardRange topCards = game.drawCards(3);
+    while (topCards.size() > 0) {
+        EXPECT_TRUE(topCards.discard(topCards(0)));
+    }
+
+    EXPECT_FALSE(topCards.discard(sh::CardType::Fascist));
+    EXPECT_FALSE(topCards.discard(sh::CardType::Liberal));
+}
+
+TEST(card_range_test, discard_applied) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    CardRange topCards = game.drawCards(3);
+    EXPECT_TRUE(topCards.applyToGame());
+    EXPECT_FALSE(topCards.discard(topCards(0)));
+}
+
+TEST(card_range_test, policy_not_found) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    CardRange topCards = game.drawCards(3);
+    while (topCards.size() > 0) {
+        EXPECT_TRUE(topCards.discard(topCards(0)));
+    }
+
+    EXPECT_FALSE(topCards.selectForPolicy(sh::CardType::Fascist));
+    EXPECT_FALSE(topCards.selectForPolicy(sh::CardType::Liberal));
+}
+
+TEST(card_range_test, policy_applied) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    CardRange topCards = game.drawCards(3);
+    EXPECT_TRUE(topCards.applyToGame());
+    EXPECT_FALSE(topCards.selectForPolicy(topCards(0)));
+}
+
+TEST(card_range_test, double_apply) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    CardRange topCards = game.drawCards(3);
+    EXPECT_TRUE(topCards.applyToGame());
+    EXPECT_FALSE(topCards.applyToGame());
+}
+
+TEST(card_range_test, raii) {
+    using namespace sh;
+    Game game({"A", "B", "C", "D", "E", "F", "G"});
+    std::vector<CardType> cardPile = game.getCardPile();
+    {
+        CardRange topCards = game.drawCards(3);
+    }
+
+    EXPECT_EQ(cardPile, game.getCardPile());
+    EXPECT_TRUE(game.getDiscardCardPile().empty());
+    EXPECT_EQ(game.getPolicies().find(CardType::Fascist)->second, 0);
+    EXPECT_EQ(game.getPolicies().find(CardType::Liberal)->second, 0);
 }
